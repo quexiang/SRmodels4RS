60fbda833fb5dfb9fdd90abc956e155900a047ae
hongyu0516 <49140483+hongyu0516@users.noreply.github.com>
2023-10-24 09:54:10 +0800

    Update inference_vapsr.py

--- Src/inference/inference_vapsr.py
+++ Src/inference/inference_vapsr.py
@@ -20,11 +20,6 @@ def writeTiff(im_data, path,im_geotrans,im_proj):
     else:
         datatype = gdal.GDT_Float32
     im_bands, im_height, im_width = im_data.shape
-    # if len(im_data.shape) == 3:
-    #     im_bands, im_height, im_width = im_data.shape
-    # elif len(im_data.shape) == 2:
-    #     im_data = np.array([im_data])
-    #     im_bands, im_height, im_width = im_data.shape
     # 创建文件
     driver = gdal.GetDriverByName("GTiff")
     dataset = driver.Create(path, int(im_width), int(im_height), int(im_bands), datatype)
@@ -64,8 +59,8 @@ def main():
         default=  # noqa: E251
         'F:/ESRGAN/basicsr-master/experiments/VapSR_X4_augment_20k/models/net_g_latest.pth'  # noqa: E501
     )
-    parser.add_argument('--input', type=str, default='F:/ESRGAN/datasets/test/input', help='input test image folder')
-    parser.add_argument('--output', type=str, default='F:/ESRGAN/datasets/inference/output_255', help='output folder')
+    parser.add_argument('--input', type=str, default='Data/Mangroves/Paired_H_L_images/LH', help='input test image folder')
+    parser.add_argument('--output', type=str, default='Data/Mangroves/inference/output', help='output folder')
     args = parser.parse_args()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -78,26 +73,7 @@ def main():
 
     os.makedirs(args.output, exist_ok=True)
 
-    # for idx, path in enumerate(sorted(glob.glob(os.path.join(args.input, '*')))):
-    #     imgname = os.path.splitext(os.path.basename(path))[0]
-    #     print('Testing', idx, imgname)
-    #     # read image
-    #     img = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
-    #     img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
-    #     img = img.unsqueeze(0).to(device)
-    #     # inference
-    #     try:
-    #         with torch.no_grad():
-    #             output = model(img)
-    #     except Exception as error:
-    #         print('Error', error, imgname)
-    #     else:
-    #         # save image
-    #         output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()
-    #         output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
-    #         output = (output * 255.0).round().astype(np.uint8)
-
-    imgname = 'LC08_ROI_QZW_unit8'
+    imgname = '1'
     path = args.input + '/' + imgname + '.tif'
 
     print('PATH', path)
@@ -105,7 +81,7 @@ def main():
         # read image
     img = gdal.Open(path)
 
-    GF = gdal.Open('I:/redo/0427ROI/GF1C_ROI_QZW.tif')
+    GF = gdal.Open('Data/Mangroves/Paired_H_L_images/HR/1.tif')
     im_geotrans = GF.GetGeoTransform()
     im_proj = GF.GetProjection() 
 
@@ -126,18 +102,7 @@ def main():
         output = output.data.squeeze().float().cpu().clamp_(0, 1).numpy()  
         output_flie = args.output + '/' +  imgname +'_VapSR.tif'
         output = (output * 255.0).round().astype(np.uint16)
-        # max_bands = [18594.0,13462.0,12476.0,12053.0]
-        # min_bands = [5586.0,5830.0,6365.0,7391.0]
-        # compress_data = np.zeros((4, h, w),dtype="uint16")
-
-        # for i in range(4):
-        #     data_band = output[i]
-        #     cutmin = float(min_bands[i])
-        #     cutmax = float(max_bands[i])
-        #     compress_data[i,:, :] = np.around((cutmax - cutmin)*data_band[:,:]/255 + cutmin)
-
         print('SIZE:', output.shape)
-        # writeTiff(compress_data, output_flie,im_geotrans,im_proj)
         writeTiff(output, output_flie,im_geotrans,im_proj)     
 
 if __name__ == '__main__':
